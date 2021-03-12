cd8afcd507b34d5d8cfc0e681bdbd46a6e597514
Bahchaev <wbahas@mail.ru>
2021-03-12 21:40:39 +0700

    first commit

---
+++ src/Components/App/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import * as json from '../../data.json'
+
+const data = json.default;
+
+function App() {
+    console.log(data);
+    return (
+        <div className="App">
+            <table>
+                <thead>
+                <tr>
+                    <th style={{width: "33%"}}>Name</th>
+                    <th style={{width: "33%"}}>Phone</th>
+                    <th style={{width: "33%"}}>Age</th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.map(obj => {
+                    return (
+                        <tr key={obj.id}>
+                            <td>{obj.name}</td>
+                            <td style={{textAlign: "center"}}>{obj.phone}</td>
+                            <td style={{textAlign: "center"}}>{obj.age}</td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+
+            </table>
+        </div>
+    );
+}
+
+export default App;
